1ae8b17b172cf0b136f1f9e92707ad3c253e83a6
tiagotunes <tiagoantunes850@gmail.com>
2026-04-11 13:10:34 +0100

    fix: change import

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -3,6 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { describe, beforeEach, it, expect } from '@jest/globals';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
